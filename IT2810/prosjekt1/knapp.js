dcc84dbce8f8734aa88a4eea75e394f745280487
trulse <fil_emil@hotmail.com>
2021-09-03 09:51:43 +0200

    fikset jquery til footer

---
+++ IT2810/prosjekt1/knapp.js
@@ -0,0 +1,16 @@
+var hiddenDiv = $("#kravDok");
+console.log("her");
+$("#button").on("click", function (event){
+
+    if(hiddenDiv.is(":hidden")){
+        hiddenDiv.show();        
+    }
+    else{
+        hiddenDiv.hide();
+    }
+
+} )
+
+//$("#knapp").click(function() {
+//    alert( "Handler for called." );
+//});
